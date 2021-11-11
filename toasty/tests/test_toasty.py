19df08a36641aaf81d8e68abb509d60194a7cec7
Henrik Norman <honke.norman@gmail.com>
2021-11-11 08:48:52 +0100

    Added tests for tile_fits

---
+++ toasty/tests/test_toasty.py
@@ -0,0 +1,42 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2021 the AAS WorldWide Telescope project
+# Licensed under the MIT License.
+
+from __future__ import absolute_import, division, print_function
+
+import pytest
+from . import test_path
+import toasty
+from shutil import rmtree
+from pathlib import Path
+
+try:
+    from astropy.io import fits
+    HAS_ASTRO = True
+except ImportError:
+    HAS_ASTRO = False
+
+
+class TestToasty(object):
+
+    @pytest.mark.skipif('not HAS_ASTRO')
+    def test_tile_fits(self):
+        out_dir_input = 'test_tiled'
+        out_dir, bld = toasty.tile_fits(
+            fits=test_path('herschel_spire.fits.gz'),
+            out_dir=out_dir_input,
+            cli_progress=True)
+        assert out_dir == out_dir_input
+        assert Path(out_dir, 'index_rel.wtml').is_file()
+        assert Path(out_dir, '0', '0', '0_0.fits').is_file()
+        rmtree(out_dir)
+
+        # Only testing with a multi tan collection, since multi WCS
+        # collections take a significant time to process
+        out_dir, bld = toasty.tile_fits(
+            fits=[test_path('wcs512.fits.gz'), test_path('wcs512.fits.gz')]
+        )
+        assert out_dir == test_path('wcs512_tiled')
+        assert Path(out_dir, 'index_rel.wtml').is_file()
+        assert Path(out_dir, '0', '0', '0_0.fits').is_file()
+        rmtree(out_dir)
